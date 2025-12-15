042e18000484f591920c2ff2c1f7b6e549cbaaa0
Y4ZK4Y <yk.yasamankarimi@gmail.com>
2025-12-15 17:39:33 +0100

    add ex00

---
+++ ex00/include/easyfind.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yasamankarimi <yasamankarimi@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 11:41:57 by yasamankari       #+#    #+#             */
+/*   Updated: 2025/08/15 11:48:38 by yasamankari      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stdexcept>
+#include <algorithm>
+
+
+
+template <typename T>
+void easyfind(T& container, int value) {
+    
+}
+
+
+template <typename T>
+void easyfind(const T& container, int value) {
+    
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yasamankarimi <yasamankarimi@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 11:40:41 by yasamankari       #+#    #+#             */
+/*   Updated: 2025/08/15 12:11:16 by yasamankari      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+/*
+Learning objectives:
+You must use the STL — especially the Containers (vector/list/map/and so forth)
+and the Algorithms(defined in header <algorithm>)
+begin() end()
+*/
+
+/*
+c++ containers
+
+*/
